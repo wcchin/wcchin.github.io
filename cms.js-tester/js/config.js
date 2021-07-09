89fb4ddacc926c15d579424b8f4931e53095028a
wcchin <wcchin.88@gmail.com>
2021-07-09 11:57:39 +0800

    modify theme

--- cms.js-tester/js/config.js
+++ cms.js-tester/js/config.js
@@ -20,7 +20,7 @@ var config = {
   },
 
   // The name of the layouts directory.
-  layoutDirectory: 'layouts',
+  layoutDirectory: 'layouts/bulma-blog',
 
   // The error layout template name.
   errorLayout: 'error',
