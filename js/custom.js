033f4c0fc879ac30a51d2a638eb8c36d70ff9b38
wcchin <wcchin.88@gmail.com>
2021-07-11 01:55:27 +0800

    reverse the publication order and hide template files

--- js/custom.js
+++ js/custom.js
@@ -9,14 +9,16 @@ function openTab(evt, tabName) {
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" is-active", "");
   }
+	//window.alert(tablinks[0].className)
 
+	//window.alert(evt.currentTarget.parentNode.className)
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+  evt.currentTarget.parentNode.className += " is-active";
 }
 
 function expandCard(evt, cardName) {
